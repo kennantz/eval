f7989b47cc3f519438a087f064f8a77330303e30
kennantz <72216837+kennantz@users.noreply.github.com>
2020-12-28 23:23:45 +0700

    Commit sasuke

--- lib/ui/pages/data_page.dart
+++ lib/ui/pages/data_page.dart
@@ -10,7 +10,7 @@ class _DataPageState extends State<DataPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("naruto"),
+        title: Text("sasuke"),
         centerTitle: true,
         leading: Container(),
       ),
