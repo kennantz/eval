3146e03480f90f687a7df4f99211858ff1549701
kennantz <72216837+kennantz@users.noreply.github.com>
2020-12-30 00:50:09 +0700

    account page

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
-      home: SignInPage()
+      home: AccountPage()
     );
   }
 
