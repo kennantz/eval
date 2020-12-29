75b0b6b3f98fc63e09f88aa269e01bbc5973153e
Christopher Kevin <ckevin634@gmail.com>
2020-12-29 23:31:41 +0700

    Cupertino Design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:eval/ui/pages/pages.dart';
 
 void main() {
@@ -13,8 +14,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
+    return CupertinoApp(
       home: SignInPage()
     );
   }
